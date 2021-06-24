3f1dbad862052a33249913002761fec8dfcdf9bb
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2021-06-24 21:55:28 +0600

    Api integrated on views.py and showed on home.html

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/historical/?format=application/json"
+                               "&zipCode=89129&date=2021-06-23T00-0000&distance=5&API_KEY=89D608E0-6419-43DD-936F"
+                               "-51E9B6C0F902")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error"
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
